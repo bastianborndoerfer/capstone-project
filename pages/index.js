eb5c4da1b604db8ae88d2f2d18ef2d2f2cb6c3b5
bastianborndoerfer <bastian.borndoerfer@gmx.de>
2023-08-14 14:50:34 +0200

    fetch data by useSWR

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
+import useSWR from "swr";
 
+const URL =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&locale=en";
 
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(URL);
+  if (error) return <div>{error.message}</div>;
+  if (isLoading) return <div>loading...</div>;
+  console.log(data);
+  
   return (
     <div>
       <h1>Hello from Next.js</h1>
