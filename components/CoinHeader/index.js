8c95e1b5ff2cba4f6c890db532f7f42eca0ad386
bastianborndoerfer <bastian.borndoerfer@gmx.de>
2023-08-17 13:00:05 +0200

    add name to CoinHeader-component

--- components/CoinHeader/index.js
+++ components/CoinHeader/index.js
@@ -2,12 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import { styled } from "styled-components";
 
-export default function CoinHeader({ image, symbol, name, price }) {
+export default function CoinHeader({ image, symbol, name }) {
   return (
     <StyledHeader>
       <Link href={"/"}>←</Link>
       <Image src={image} alt={name} height={80} width={80} />
-      <p>{symbol}</p>
+      <p>{name}</p>
+      <p>({symbol})</p>
     </StyledHeader>
   );
 }
