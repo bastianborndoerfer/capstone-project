14232eae49487ed7ca39526b501b05bdd32b3a3e
bastianborndoerfer <bastian.borndoerfer@gmx.de>
2023-08-31 01:14:52 +0200

    add index to Holdings

--- components/Holdings/index.js
+++ components/Holdings/index.js
@@ -2,22 +2,27 @@ import styled from "styled-components";
 
 export default function Holdings({ positions }) {
   return (
-    <ul>
-      {positions.map((position) => (
+    <StyledUl>
+      {positions.map((position, index) => (
         <StyledList key={position.id}>
+          <p>{index + 1}.</p>
           <p>Price: {position.price}</p>
           <p>Quantity: {position.quantity}</p>
           <p>Total: {position.total}</p>
           <p>Date: {position.date}</p>
         </StyledList>
       ))}
-    </ul>
+    </StyledUl>
   );
 }
 
-
+const StyledUl = styled.ul`
+padding: 0;
+`;
 
 const StyledList = styled.li`
+  padding: 10px;
+  margin-bottom: 10px;
   display: flex;
   justify-content: space-evenly;
   border: 1px black solid;
